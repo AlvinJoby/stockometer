f49af75c9a56182d7652bb773da421a814ef0891
Alvin Joby <alvinjoby100@gmail.com>
2026-03-01 11:14:57 +0530

    updated graph

--- graph.py
+++ graph.py
@@ -10,17 +10,30 @@ def generate_graph(data,symbol):
         x=data.index,
         y=data[colname(symbol,"Close")],
         mode="lines",
-        name="Close Price"
+        name="Close Price",
+        hovertemplate="%{y:.2f}<extra></extra>"
     ))
 
     fig.update_layout(
-        title=f"{symbol} Close Price",
-        xaxis_title="Date",
-        yaxis_title="Price",
-        template="plotly_dark"
+        #title=dict(text=f"{symbol}",
+        #           font=dict(size=26)),
+        #xaxis_title="Date",
+        #yaxis_title="Price",
+        yaxis=dict(side="right",tickformat=".2f"),
+        template="plotly_dark",
+        dragmode="pan",
+        hovermode="x unified",
+        height=600,
+        margin=dict(l=20,r=30)
     )
 
-    graph_html = pio.to_html(fig, full_html=False)
+    graph_html = pio.to_html(fig,
+                             full_html=False,
+                             config={
+                                 "scrollZoom":True,
+                                 "displaylogo":False,
+                                 "displayModeBar": False
+                             })
 
     return graph_html
 
